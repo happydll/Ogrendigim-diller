0a18034bce3b61d8eb9a5937acaf922639300949
happydll <ibrahimyasininan@gmail.com>
2022-01-25 22:37:55 +0300

    .

---
+++ Python/Uygulamalar/1/1.py
@@ -0,0 +1,22 @@
+#Değişken Değerini Değiştirmek
+
+x=10
+y=20
+
+#print("x= "+ str(x))
+#print("y= "+ str(y))
+
+
+#bunu y=10 x=20 olarak yazdır
+
+#çözüm
+
+temp=x
+x=y
+y=temp
+
+print("x= "+ str(x))
+print("y= "+ str(y))
+
+
+
---
+++ Python/Uygulamalar/2/2.py
@@ -0,0 +1,7 @@
+#Kaç kilometre kaç mil eder ?
+
+km=float(input("Dönüştüreceğiniz kilometre değerini yazınız:"))
+
+mil=km*0.621371192
+
+print(str(km) +" "+"kilometre"+" "+str(mil)+" "+"mil eder.")
